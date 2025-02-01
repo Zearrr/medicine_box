5e7029be175dbbe0762830a85c4965ee3ce99cff
Krorakot <6504305001308@student.sru.ac.th>
2025-02-02 02:30:34 +0700

    Update

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+
+// JavaScript สำหรับการสลับระหว่างโหมดกลางคืนและกลางวัน
+const themeSwitch = document.getElementById('themeSwitch');
+
+themeSwitch.addEventListener('change', function() {
+    if (themeSwitch.checked) {
+        document.body.classList.remove('day-mode');
+        document.body.classList.add('night-mode');
+    } else {
+        document.body.classList.remove('night-mode');
+        document.body.classList.add('day-mode');
+    }
+});
+
+// ตั้งค่าค่าเริ่มต้นของโหมด (เช่น โหมดกลางวัน)
+if (!themeSwitch.checked) {
+    document.body.classList.add('day-mode');
+}
+
+
 window.addEventListener("load", function () {
     document.querySelector(".menu").classList.add("show");
 });
